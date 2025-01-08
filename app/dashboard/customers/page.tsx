34dec0fcfea99df35475ce519a572cf6002dbaf2
Umer Farooq <mumerfarooq557@gmail.com>
2025-01-08 18:53:58 +0500

    check

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Customers() {
-  return <div>Customers</div>;
+  return (
+    <div>
+      Customers
+      <p className="text-red-500 bg-black">{process.env.PGDATABASE}</p>
+    </div>
+  );
 }
 
 export default Customers;
